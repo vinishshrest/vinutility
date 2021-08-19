550aea97eb2eb0c030f1e949de0c72dea51e63e5
vinishshrest <shresthavinish@gmail.com>
2021-08-19 00:54:21 -0400

    package_dl_function

---
+++ R/install_packages.R
@@ -0,0 +1,12 @@
+# Install packages not yet installed
+fun_pack <- function(packages) {
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+else{
+  print("installed")
+}
+}
+
+
